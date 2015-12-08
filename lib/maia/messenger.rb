45ef335b6eed41409fd06bf656d499da4f662f7a
Logan Serman <logan.serman@metova.com>
2015-12-08 16:41:35 -0600

    Log all failed tokens and error messages

--- lib/maia/messenger.rb
+++ lib/maia/messenger.rb
@@ -19,17 +19,21 @@ module Maia
     end
 
     private
+      def gcm
+        @_service ||= GCM::Service.new connection: connection
+      end
+
+      def connection
+        nil # use the default
+      end
+
       def handle_failed_tokens(results)
         results.each do |result|
           device = Maia::Device.find_by(token: result.token)
-          case result.error
-          when /InvalidRegistration/, /NotRegistered/
+          logger.info "#{result.token} failed: #{result.error}"
+          if device && invalid_token_error?(result.error)
             logger.info "Invalid token #{device.token} - destroying device #{device.id}."
-            device.destroy if device
-          when /MessageTooBig/
-            logger.info 'Message sent was too large.'
-          when /Timeout/
-            logger.info 'Request to GCM timed out'
+            device.destroy
           end
         end
       end
@@ -46,12 +50,8 @@ module Maia
         end
       end
 
-      def gcm
-        @_service ||= GCM::Service.new connection: connection
-      end
-
-      def connection
-        nil # use the default
+      def invalid_token_error?(error)
+        error =~ /InvalidRegistration|NotRegistered/
       end
 
       def user_already_has_token_registered?(user, token)
